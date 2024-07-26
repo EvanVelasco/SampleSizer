d853c930f088e2a58cce74de5cfdb13e421e33c8
Evan Velasco <evan.velasco@live.com>
2024-07-25 23:02:18 -0400

    Added third tab for proportions

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 from tabs.tab1 import show_tab1
 from tabs.tab2 import show_tab2
+from tabs.tab3 import show_tab3
 
 def main():
-    tab1, tab2 = st.tabs(["SRS-Total", "SRS-Mean"])
+    tab1, tab2, tab3 = st.tabs(["SRS-Total", "SRS-Mean", "SRS-Proportion"])
 
     with tab1:
         show_tab1()
     with tab2:
         show_tab2()
+    with tab3:
+        show_tab3()
 
 
 if __name__ == "__main__":
